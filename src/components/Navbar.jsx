e42e6282892e503a29482dbaa3c3269f87c726c0
Chris Mittell <mittell@hotmail.co.uk>
2022-11-01 09:48:18 +0900

    chore: updated link colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,7 +67,7 @@ const Navbar = () => {
 							Twitter <FaTwitter size={30} />
 						</a>
 					</li>
-					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-black'>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
 						<a
 							href='/'
 							className='flex justify-between items-center w-full text-gray-300'
