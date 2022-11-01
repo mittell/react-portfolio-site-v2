4dee9324d41eaaf117c0576dd70fd5d2828a0ddf
Chris Mittell <mittell@hotmail.co.uk>
2022-11-01 10:08:19 +0900

    chore: updated spelling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 					Chris Mittell
 				</h1>
 				<h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-					I`m a Full-Stack Developer.
+					I`m a Full-Stack Developer
 				</h2>
 				<p className='text-[#8892b0] py-4 max-w-[700px]'>
 					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
